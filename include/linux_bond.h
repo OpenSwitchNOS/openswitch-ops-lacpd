1d212b11d862f5537548c33528d07f0ccdfb6e85
Agustin Meneses <agustin-jose.meneses-fuentes@hpe.com>
2016-02-11 15:36:51 -0800

    new: dev: Add Linux bonding driver to lacpd @wip
    Add new code to use Linux bonding driver to create and configure bonding interfaces for each configured LAG.
    Note: This work is still in progress. Right now slaves are being added to the bond but they are not being removed.
    Also, the idea is to remove system calls and use netlink to configure the bonding driver.
    
    Change-Id: Id5628b31e3b16d6881d7ccc3f24d3fc971be019f
    Signed-off-by: Agustin Meneses <agustin-jose.meneses-fuentes@hpe.com>

---
+++ include/linux_bond.h
@@ -0,0 +1,24 @@
+/*
+ * (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+void load_bonding_driver();
+void delete_linux_bond(char* bond_name);
+void create_linux_bond(char* bond_name);
+void add_slave_to_bond(char* bond_name, char* slave_name);
+void remove_slave_from_bond(char* bond_name, char* slave_name);
+void set_linux_bond_up(char* bond_name);
+void set_slave_interface_down(char* slave_name);
+void set_slave_interface_up(char* slave_name);
