b9472ea8a1c1233393570bc77be5cfc46bce74a2
dherdoiza <david.herdoiza@hpe.com>
2016-03-14 07:03:27 -0700

    new: dev: Add FT for shutdown/no shutdown in LAG
    Add FT to verify the correct functionality in the commands
    shutdown and no shutdown for LAG
    
    Change-Id: Ia7401d3c6a03eb58204369f38a73b38c77fe2808
    Signed-off-by: dherdoiza <david.herdoiza@hpe.com>

---
+++ ops-tests/feature/ft_lacp_lib.py
@@ -0,0 +1,106 @@
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+##########################################################################
+# Name:        ft_lacp_lib.py
+#
+# Objective:   Library for all utils function used across all LACP tests
+#
+# Topology:    N/A
+#
+##########################################################################
+
+"""
+OpenSwitch Test Library for LACP
+"""
+
+
+def create_lag_off(sw, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.no_routing()
+
+
+def delete_lag(sw, lag_id):
+    with sw.libs.vtysh.Configure() as ctx:
+        ctx.no_interface_lag(lag_id)
+
+
+def associate_interface_to_lag(sw, interface, lag_id):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.lag(lag_id)
+
+
+def turn_on_interface(sw, interface):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.no_shutdown()
+
+
+def turn_off_interface(sw, interface):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.shutdown()
+
+
+def is_interface_up(sw, interface):
+    interface_status = sw('show interface {interface}'.format(**locals()))
+    lines = interface_status.split('\n')
+    for line in lines:
+        if "Admin state" in line and "up" in line:
+            return True
+    return False
+
+
+def create_vlan(sw, vlan_id):
+    with sw.libs.vtysh.ConfigVlan(vlan_id) as ctx:
+        ctx.no_shutdown()
+
+
+def associate_vlan_to_lag(sw, vlan_id, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.vlan_access(vlan_id)
+
+
+def associate_vlan_to_l2_interface(sw, vlan_id, interface):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(vlan_id)
+
+
+def check_connectivity_between_hosts(h1, h1_ip, h2, h2_ip, success):
+    ping = h1.libs.ping.ping(1, h2_ip)
+    if success:
+        assert ping['transmitted'] == ping['received'] == 1,\
+            'Ping between ' + h1_ip + ' and ' + h2_ip + ' failed'
+    else:
+        assert not ping['transmitted'] == ping['received'] == 1,\
+            'Ping between ' + h1_ip + ' and ' + h2_ip + ' success'
+
+    ping = h2.libs.ping.ping(1, h1_ip)
+    if success:
+        assert ping['transmitted'] == ping['received'] == 1,\
+            'Ping between ' + h2_ip + ' and ' + h1_ip + ' failed'
+    else:
+        assert not ping['transmitted'] == ping['received'] == 1,\
+            'Ping between ' + h2_ip + ' and ' + h1_ip + ' success'
+
+
+def lag_shutdown(sw, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.shutdown()
+
+
+def lag_no_shutdown(sw, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.no_shutdown()
