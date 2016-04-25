5fea808a30c71f89365809a13b5ec2d415df60b6
Erick Castro <erick.castro@hpe.com>
2016-04-25 12:19:33 -0700

    fix: dev: Taiga defect 646
    Repeated column in LACP CLI "show lacp interfaces" command
    
    Change-Id: I7d721e2ffc0733e45a54941e7bf35f41647e2495
    Signed-off-by: Erick Castro <erick.castro@hpe.com>

--- tests/test_vtysh_ct_lacp.py
+++ tests/test_vtysh_ct_lacp.py
@@ -611,13 +611,13 @@ class LACPCliTest(OpsVsiTest):
         out = s1.cmdCLI('show lacp interface')
         lines = out.split('\n')
         for line in lines:
-            if 'Intf Aggregate Port    Port     Key  State   '\
+            if 'Intf Aggregate Port    Port     State   '\
                'System-id         System   Aggr' in line:
                 success += 1
             if 'name      id      Priority                                '\
                'Priority Key' in line:
                 success += 1
-            if 'Intf Aggregate Partner Port     Key  State   '\
+            if 'Intf Aggregate Partner Port     State   '\
                'System-id         System   Aggr' in line:
                 success += 1
             if 'name      Port-id Priority                                '\
